9f1e65ec8a2c031cbe4a1daea01d3612118905a6
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-05-08 11:39:55 -0500

    cambios en el controlador es

--- application/controllers/Es.php
+++ application/controllers/Es.php
@@ -3,8 +3,34 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Es extends CI_Controller {
 
+	function __construct() {
+        parent::__construct();
+        $this->load->model('M_solicitud');
+        $this->load->helper('utils');
+        $this->load->helper("url");//BORRAR CACHÉ DE LA PÁGINA
+        $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
+        $this->output->set_header('Cache-Control: post-check=0, pre-check=0',false);
+        $this->output->set_header('Pragma: no-cache');
+    }
+
 	public function index()
 	{
-		$this->load->view('v_es');
+		$data['nombre'] = '';
+		$this->load->view('v_es', $data);
 	}
+
+	function cambiarIdioma(){
+    $data['error'] = EXIT_ERROR;
+      $data['msj'] = null;
+      try {
+        $idioma  = $this->input->post('idioma');
+        $session = array('idioma' => $idioma);
+        $this->session->set_userdata($session);
+        $data['error'] = EXIT_SUCCESS;
+      }catch(Exception $e) {
+        $data['msj'] = $e->getMessage();
+      }
+      echo json_encode($data);
+  }
 }
