5538904d6959cead91be3981cd5de81d5b70c482
joseminaya15 <jose.minayac15@gmail.com>
2018-05-08 10:42:37 -0500

    primera pantalla cloud

--- application/views/v_es.php
+++ application/views/v_es.php
@@ -19,16 +19,16 @@
         <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>font-awesome.min.css?v=<?php echo time();?>">
         <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>material-icons.css?v=<?php echo time();?>">
         <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>bentonsans.css?v=<?php echo time();?>">
-    	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>animate.css?v=<?php echo time();?>">
+    	<!-- <link rel="stylesheet"    href="<?php echo RUTA_CSS?>animate.css?v=<?php echo time();?>"> -->
     	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
     	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>style.css?v=<?php echo time();?>">
     </head>
     <body>
         <section id="principal">
-            <div id="home" class="window-center">
-                <div class="header-home">
-                    <img src="<?php echo RUTA_IMG?>logo/logo_home.png">
-                    <div class="mdl-idioma">
+            <div id="home" class="js-window">
+                <div class="js-header">
+                    <img src="<?php echo RUTA_IMG?>logo/logo-sap__run.png">
+                    <div class="js-idioma">
                         <select class="selectpicker" id="IdiomaHome"  name="Idioma" onchange="cambiarIdiomaHome()">
                             <option value="Español">Espa&ntilde;ol</option>
                             <option value="Inglés">English</option>
@@ -36,22 +36,37 @@
                         </select>
                     </div>
                 </div>
-                <div class="mdl-container mdl-all-window">
-                    <img class="logo-home" src="<?php echo RUTA_IMG?>logo/logo_header.png">
-                    <h2 class="title-home">Diferentes escenarios. Una soluci&oacute;n</h2>
+                <div class="js-title">
+                    <h2>Bienvenido a <br><strong>Cloud Configurator</strong></h2>
+                    <p>Con esta herramienta podr&aacute; obtener r&aacute;pidamente una configuraci&oacute;n personalizada de las Ofertas SAP Cloud en funci&oacute;n a sus necesidades</p>
                 </div>
-                <div class="background-body">
-                    <div class="background-body3"></div>
-                    <div class="background-body2"></div>
-                    <div class="background-body1"></div>
+                <div class="js-bar">
+                    <div class="js-bar__children background-body3"></div>
+                    <div class="js-bar__children background-body2"></div>
+                    <div class="js-bar__children background-body1"></div>
                 </div>
-                <div class="button-next-prev">
-                    <button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect button-arrow button-next" onclick="buttonNext()">
-                        <div class="arrow arrow-right"></div>
+                <div class="js-fondo__imagen"></div>
+                <div class="js-button-next">
+                    <button class="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab" onclick="buttonNext()">
+                        <i class="mdi mdi-keyboard_arrow_down"></i>
                     </button>
                 </div>
-                <div class="fondo-imagen"></div>
+            </div>
+            <!-- TODOS LOS CARDS  -->
+            <div id="">
+                
             </div>
         </section> 
+
+        <script src="<?php echo RUTA_JS?>jquery-3.2.1.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>jquery-1.11.2.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap/js/bootstrap.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/bootstrap-select.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_PLUGINS?>toaster/toastr.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>jsutils.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>jsmenu.js?v=<?php echo time();?>"></script>
+        <script src="<?php echo RUTA_JS?>jsindex_es.js?v=<?php echo time();?>"></script>
     </body>
 </html>
