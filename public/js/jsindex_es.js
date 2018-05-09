e3618917d9fde15f5b45ed0650358204537df78b
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-05-09 10:09:23 -0500

    cambiar idioma

--- public/js/jsindex_es.js
+++ public/js/jsindex_es.js
@@ -5,3 +5,28 @@ function buttonNext(){
 	homePage.addClass('animated fadeOutUp');
 	congigurator.addClass('animated fadeInDown');
 }
+function cambiarIdioma(){
+	var idioma = $('#IdiomaHome').val();
+	if(idioma == 'Español'){
+		location.href = 'Es';
+	}else if(idioma == 'Inglés'){
+		location.href = 'En';
+	}else if(idioma == 'Portugués'){
+		location.href = 'Pt';
+	}
+	$.ajax({
+		data : {idioma : idioma},
+		url  : 'es/cambiarIdioma',
+		type : 'POST'
+	}).done(function(data){
+		try{
+        data = JSON.parse(data);
+        if(data.error == 0){
+        }else{
+        	return;
+        }
+      } catch (err){
+        msj('error',err.message);
+      }
+	});
+}
