db02a24f72f964ef2a6115e317bd227257615bae
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-05-09 12:25:29 -0500

    cambios en ingles

--- application/controllers/En.php
+++ application/controllers/En.php
@@ -3,8 +3,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class En extends CI_Controller {
 
-	public function index()
-	{
-		$this->load->view('v_en');
+	function __construct() {
+        parent::__construct();
+        $this->load->model('M_solicitud');
+        $this->load->helper("url");//BORRAR CACHÉ DE LA PÁGINA
+        $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
+        $this->output->set_header('Cache-Control: post-check=0, pre-check=0',false);
+        $this->output->set_header('Pragma: no-cache');
+    }
+
+	public function index(){
+		$data['nombre'] = '';
+		$this->load->view('v_en', $data);
 	}
+	function cambiarIdioma(){
+    $data['error'] = EXIT_ERROR;
+      $data['msj'] = null;
+      try {
+        $idioma  = $this->input->post('idioma');
+        $session = array('idioma' => $idioma);
+        $this->session->set_userdata($session);
+        $data['error'] = EXIT_SUCCESS;
+      }catch(Exception $e) {
+        $data['msj'] = $e->getMessage();
+      }
+      echo json_encode($data);
+  }
 }
