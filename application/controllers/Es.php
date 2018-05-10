6d473d906ea9da19293d7d8d861b11e7f702db0f
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-05-10 11:15:23 -0500

    cambios en e controlador es

--- application/controllers/Es.php
+++ application/controllers/Es.php
@@ -502,4 +502,27 @@ class Es extends CI_Controller {
       }
       echo json_encode($data);
   }
+  function encontrarDatos(){
+    $data['error'] = EXIT_ERROR;
+    $data['msj']   = null;
+    try {
+      $html  = '';
+      $texto = $this->input->post('texto');
+      if($texto == '' || $texto == null || $texto == undefined){
+        throw new Exception("Error Processing Request", 1);
+      }
+      $datos = $this->M_solicitud->getDatos();
+      foreach($datos as $row){
+        $html  .= '<tr>
+                      <td></td>
+                      <td></td>
+                      <td></td>
+                   </tr>';
+      }
+      $data['error'] = EXIT_SUCCESS;
+    }catch(Exception $e){
+      $data['msj'] = $e->getMessage();
+    }
+    echo json_encode($data);
+  }
 }
