b9761b39c5cb0c97416780402595cae2327db117
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-05-16 09:53:38 -0500

    cambios en el controlador es

--- application/controllers/Es.php
+++ application/controllers/Es.php
@@ -4,13 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Es extends CI_Controller {
 
 	function __construct() {
-        parent::__construct();
-        $this->load->model('M_solicitud');
-        $this->load->helper("url");
-        $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
-        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
-        $this->output->set_header('Cache-Control: post-check=0, pre-check=0',false);
-        $this->output->set_header('Pragma: no-cache');
+      parent::__construct();
+      $this->load->model('M_solicitud');
+      $this->load->helper("url");
+      $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
+      $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
+      $this->output->set_header('Cache-Control: post-check=0, pre-check=0',false);
+      $this->output->set_header('Pragma: no-cache');
   }
 	public function index(){
     $data['nombre_comple']   = $this->session->userdata('nombre_linke');
