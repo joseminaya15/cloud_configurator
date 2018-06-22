41a86fdf7963d78922d94dd02c12070e9b60a2d7
joseminaya15 <jose.minayac15@gmail.com>
2018-06-22 14:32:31 -0500

    sadad

--- application/views/v_es.php
+++ application/views/v_es.php
@@ -24,6 +24,8 @@
     	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>animate.css?v=<?php echo time();?>">
     	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
     	<link rel="stylesheet"    href="<?php echo RUTA_CSS?>style.css?v=<?php echo time();?>">
+        <!--Google Tag Manager --><script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P W9PLW6');</script><!--End Google Tag Manager -->
+    <!--Google Tag Manager  (noscript) --><noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P W9PLW6"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript><!--End Google Tag Manager  (noscript) -->
     </head>
     <body>
         <section id="principal">
